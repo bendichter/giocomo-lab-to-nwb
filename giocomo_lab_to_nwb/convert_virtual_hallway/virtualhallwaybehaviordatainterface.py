401ac80dd79f13532e24f05811a36e8ce05475f7
weiglszonja <wszonja@gmail.com>
2021-02-09 18:02:00 +0100

    Add behavior interface for virtual hallway data

--- giocomo_lab_to_nwb/convert_virtual_hallway/virtualhallwaybehaviordatainterface.py
+++ giocomo_lab_to_nwb/convert_virtual_hallway/virtualhallwaybehaviordatainterface.py
@@ -1,11 +1,16 @@
 """Authors: Szonja Weigl, Luiz Tauffer and Ben Dichter."""
 from pathlib import Path
 
+from hdmf.backends.hdf5 import H5DataIO
 from nwb_conversion_tools.basedatainterface import BaseDataInterface
 from nwb_conversion_tools.utils import get_base_schema, get_schema_from_hdmf_class
-from pynwb.behavior import SpatialSeries
-from pynwb import NWBFile
+from pynwb.behavior import SpatialSeries, BehavioralEvents
+from pynwb import NWBFile, TimeSeries
 from scipy.io import loadmat
+import numpy as np
+
+from ndx_labmetadata_giocomo import LabMetaData_ext
+from ..utils import check_module
 
 
 class VirtualHallwayDataInterface(BaseDataInterface):
@@ -31,9 +36,85 @@ class VirtualHallwayDataInterface(BaseDataInterface):
         return metadata_schema
 
     def run_conversion(self, nwbfile: NWBFile, metadata: dict):
-        """TODO"""
+        """Conversion function for the Virtual Hallway task behavior interface."""
+
+        # Add lab metadata to nwbfile
+        if 'lab_meta_data' in metadata:
+            if 'neurodata_type' in metadata['lab_meta_data']:
+                metadata['lab_meta_data'].pop('neurodata_type')
+            lab_meta_data = LabMetaData_ext(**metadata['lab_meta_data'])
+            nwbfile.add_lab_meta_data(lab_meta_data)
+
         mat_file_name = self.source_data['file_path']
-
         if Path(mat_file_name).is_file():
-            visual_hallway_data = loadmat(mat_file_name, struct_as_record=False, squeeze_me=True)
+            virtual_hallway_data = loadmat(mat_file_name, struct_as_record=False, squeeze_me=True)
 
+            # Add trial information
+            trials = virtual_hallway_data['trial']
+            times = virtual_hallway_data['post']
+            trial_nums = np.unique(trials)
+            for trial_num in trial_nums:
+                nwbfile.add_trial(start_time=times[trials == trial_num][0],
+                                  stop_time=times[trials == trial_num][-1])
+
+            # matlab trial numbers start at 1. To correctly index trial_contract vector,
+            # subtracting 1 from 'trial_num' so index starts at 0
+            trial_contrast = [virtual_hallway_data['trial_contrast'][num - 1] for num in trial_nums]
+            nwbfile.add_trial_column(name='trial_contrast',
+                                     description='visual contrast of the maze through '
+                                                 'which the mouse is running',
+                                     data=trial_contrast)
+
+            # Add mouse virtual position as TimeSeries object
+            virtual_position = virtual_hallway_data['posx']
+            sampling_rate = 1 / (times[1] - times[0])
+            position_ts = TimeSeries(name='VirtualPosition',
+                                     data=H5DataIO(virtual_position, compression="gzip"),
+                                     unit='centimeter',
+                                     conversion=0.01,
+                                     resolution=np.nan,
+                                     rate=sampling_rate,
+                                     starting_time=times[0],
+                                     description='Subject position in the virtual hallway.',
+                                     comments='Values should be between 0 and 400cm. '
+                                              'Values greater than 400cm mean that the mouse'
+                                              ' briefly exited the maze.')
+
+            # Add mouse physical position
+            physical_position = []
+            for trial_num in trial_nums:
+                trial_position = virtual_position[trials == trial_num]
+                trial_gain = virtual_hallway_data['trial_gain'][trial_num - 1]
+                physical_position.extend(trial_position / trial_gain)
+
+            physical_position_ts = TimeSeries(name='PhysicalPosition',
+                                              data=H5DataIO(physical_position, compression="gzip"),
+                                              unit='centimeter',
+                                              conversion=0.01,
+                                              resolution=np.nan,
+                                              rate=sampling_rate,
+                                              starting_time=times[0],
+                                              description='Physical location on the wheel measured'
+                                                          ' since the beginning of the trial.',
+                                              comments='Physical location found by dividing '
+                                                       'the virtual position by the trial_gain.')
+
+            # Add lick events
+            events_ts = TimeSeries(name='LickEvents',
+                                        data=H5DataIO(virtual_hallway_data['lickx'],
+                                                      compression="gzip"),
+                                        unit='centimeter',
+                                        resolution=np.nan,
+                                        timestamps=H5DataIO(virtual_hallway_data['lickt'],
+                                                            compression="gzip"),
+                                        description='Subject position in virtual hallway '
+                                                    'during the lick.')
+
+            lick_events = BehavioralEvents(name='BehavioralEvents',
+                                           time_series=events_ts)
+
+            behavioral_processing_module = check_module(nwbfile, 'behavior',
+                                                        'contains processed behavioral data')
+            behavioral_processing_module.add_data_interface(position_ts)
+            behavioral_processing_module.add_data_interface(physical_position_ts)
+            behavioral_processing_module.add_data_interface(lick_events)
