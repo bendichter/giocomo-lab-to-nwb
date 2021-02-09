c0d474dbb14df800d27af92a10d2a24adc8fc540
weiglszonja <wszonja@gmail.com>
2021-02-09 20:16:45 +0100

    Add check module function to data interface

--- giocomo_lab_to_nwb/convert_virtual_hallway/virtualhallwaybehaviordatainterface.py
+++ giocomo_lab_to_nwb/convert_virtual_hallway/virtualhallwaybehaviordatainterface.py
@@ -10,7 +10,29 @@ from scipy.io import loadmat
 import numpy as np
 
 from ndx_labmetadata_giocomo import LabMetaData_ext
-from ..utils import check_module
+
+
+def check_module(nwbfile, name, description=None):
+    """
+    Check if processing module exists. If not, create it. Then return module.
+
+    Parameters
+    ----------
+    nwbfile: pynwb.NWBFile
+    name: str
+    description: str | None (optional)
+
+    Returns
+    -------
+    pynwb.module
+
+    """
+    if name in nwbfile.modules:
+        return nwbfile.modules[name]
+    else:
+        if description is None:
+            description = name
+        return nwbfile.create_processing_module(name, description)
 
 
 class VirtualHallwayDataInterface(BaseDataInterface):
