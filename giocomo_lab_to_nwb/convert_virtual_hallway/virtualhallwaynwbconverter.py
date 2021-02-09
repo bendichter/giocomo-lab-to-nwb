fa2080fa641417ce1dd0334b51db3a4c5ea30988
weiglszonja <wszonja@gmail.com>
2021-02-09 18:03:30 +0100

    Add metadata converter

--- giocomo_lab_to_nwb/convert_virtual_hallway/virtualhallwaynwbconverter.py
+++ giocomo_lab_to_nwb/convert_virtual_hallway/virtualhallwaynwbconverter.py
@@ -1,5 +1,4 @@
 """Authors: Szonja Weigl, Luiz Tauffer and Ben Dichter."""
-from datetime import datetime
 from pathlib import Path
 
 from nwb_conversion_tools import NWBConverter, SpikeGLXRecordingInterface
@@ -15,26 +14,14 @@ class VirtualHallwayNWBConverter(NWBConverter):
     )
 
     def get_metadata(self):
-        """Load task specific metadata from .meta file"""
-
         metadata = super().get_metadata()
+        behavior_file_path = Path(self.data_interface_objects['VirtualHallwayData'].source_data['file_path'])
+        session_id = behavior_file_path.stem
 
-        bin_file_path = Path(self.data_interface_objects['SpikeGLXRecording'].source_data['file_path'])
-        meta_file_path = bin_file_path.with_suffix('.meta')
-
-        if meta_file_path.is_file():
-            with meta_file_path.open('r') as meta_file:
-                metadata_from_bin = meta_file.read().splitlines()
-
-            metadata_dict = {x.split('=')[0]: x.split('=')[1] for x in metadata_from_bin}
-            file_create_date = metadata_dict['fileCreateTime']
-            session_start_time = datetime.fromisoformat(file_create_date)
-
-            metadata['NWBFile'].update(
-                session_start_time=session_start_time,
-            )
-
-        else:
-            print(f"Warning: no meta file detected at {bin_file_path.parent}!")
+        metadata['NWBFile'].update(
+            institution='Stanford University School of Medicine',
+            lab='Giocomo',
+            session_id=session_id
+        )
 
         return metadata
