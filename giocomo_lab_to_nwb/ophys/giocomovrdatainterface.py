23da8a3918d0b595dc6902c0e5cdc77b364370eb
Saksham Sharda <saksham20.sharda@gmail.com>
2021-02-10 20:57:04 +0530

    add tz, virus, surgery info

--- giocomo_lab_to_nwb/ophys/giocomovrdatainterface.py
+++ giocomo_lab_to_nwb/ophys/giocomovrdatainterface.py
@@ -55,20 +55,30 @@ class GiocomoVRInterface(BaseDataInterface):
     def get_metadata(self):
         exp_desc = self.file_path.parents[0].name
         date = self.file_path.parents[1].name
+        time_zone = timezone('US/Pacific')
         subject_num = self.file_path.parents[2].name
         session_desc = self.file_path.stem
-        subject_details = subject_info[subject_num]
+        subject_info_path = Path(__file__).parent/'subjectdata.json'
+        with open(str(subject_info_path), 'r') as js:
+            all_sub_details = json.load(js)
+        subject_details = all_sub_details[subject_num]
         metadata = dict(
             NWBFile=dict(
                 session_description=session_desc,
                 identifier=str(uuid.uuid4()),
-                session_start_time=datetime.strptime(date, "%m_%d_%Y"),
-                experiment_description=exp_desc
+                session_start_time=datetime.strptime(date, "%m_%d_%Y").astimezone(time_zone),
+                experiment_description=exp_desc,
+                virus=f'virus injection date: {subject_details["virus injection date"]}, '
+                      f'virus: {subject_details["VIRUS"]}',
+                surgery=f'cannula implant date: {subject_details["cannula implant date"]}',
+                lab='GiocomoLab',
+                institution='Stanford University School of Medicine',
+                experimenter='Mark Plitt'
             ),
             Subject=dict(
                 subject_id=subject_details['ID'],
                 species=subject_details['species'],
-                date_of_birth=subject_details['DOB'],
+                date_of_birth=datetime.strptime(subject_details['DOB'], "%Y-%m-%d %H:%M:%S").astimezone(time_zone),
                 genotype=subject_details['genotype'],
                 sex=subject_details['sex'],
                 weight=subject_details['weight at time of implant']
@@ -112,72 +122,6 @@ class GiocomoVRInterface(BaseDataInterface):
         # adding stimulus:
         for inp_kwargs in self.stimulus_args:
             if inp_kwargs['name'] not in nwbfile.stimulus:
-                inp_kwargs.update(starting_time=start_time, rate=rate, data=self.data_frame[inp_kwargs['name']].to_numpy())
+                inp_kwargs.update(starting_time=start_time, rate=rate,
+                                  data=self.data_frame[inp_kwargs['name']].to_numpy())
                 nwbfile.add_stimulus(TimeSeries(**inp_kwargs))
-
-
-subject_info = \
-    {'4139265.3': {'mouse #': '4139265.3', 'species': 'mus musculus', 'ID': 'R1', 'DOB': '2018-11-07 00:00:00',
-                   'genotype': 'CaMKII-cre hemizygous', 'sex': 'MALE', 'virus injection date': '2018-12-20 00:00:00',
-                   'VIRUS': 'AAV-PHP.eB-EF1a-DIO-GCaMP6f (retro-orbital injection)',
-                   'cannula implant date': '2019-01-09 00:00:00', 'weight at time of implant': '24.1 g'},
-     '4139265.4': {'mouse #': '4139265.4', 'species': 'mus musculus', 'ID': 'R2', 'DOB': '2018-11-07 00:00:00',
-                   'genotype': 'CaMKII-cre hemizygous', 'sex': 'MALE', 'virus injection date': '2018-12-20 00:00:00',
-                   'VIRUS': 'AAV-PHP.eB-EF1a-DIO-GCaMP6f (retro-orbital injection)',
-                   'cannula implant date': '2019-01-09 00:00:00', 'weight at time of implant': '23.0 g'},
-     '4139265.5': {'mouse #': '4139265.5', 'species': 'mus musculus', 'ID': 'R3', 'DOB': '2018-11-07 00:00:00',
-                   'genotype': 'CaMKII-cre hemizygous', 'sex': 'MALE', 'virus injection date': '2018-12-20 00:00:00',
-                   'VIRUS': 'AAV-PHP.eB-EF1a-DIO-GCaMP6f (retro-orbital injection)',
-                   'cannula implant date': '2019-01-09 00:00:00', 'weight at time of implant': '22.9 g'},
-     '4222168.1': {'mouse #': '4222168.1', 'species': 'mus musculus', 'ID': 'R4', 'DOB': '2019-03-03 00:00:00',
-                   'genotype': 'CaMKII-cre hemizygous', 'sex': 'FEMALE', 'virus injection date': '2019-07-17 00:00:00',
-                   'VIRUS': 'AAV1-CAG-FLEX-GCaMP6f-WPRE', 'cannula implant date': '2019-07-17 00:00:00',
-                   'weight at time of implant': '18.6 g'},
-     '4343703.1': {'mouse #': '4343703.1', 'species': 'mus musculus', 'ID': 'R5', 'DOB': '2019-10-29 00:00:00',
-                   'genotype': 'CaMKII-cre hemizygous', 'sex': 'MALE', 'virus injection date': '2020-02-20 00:00:00',
-                   'VIRUS': 'AAV1-CAG-FLEX-GCaMP6f-WPRE', 'cannula implant date': '2020-02-20 00:00:00',
-                   'weight at time of implant': '29.8 g'},
-     '4343706.0': {'mouse #': '4343706.0', 'species': 'mus musculus', 'ID': 'R6', 'DOB': '2019-12-19 00:00:00',
-                   'genotype': 'WT', 'sex': 'MALE', 'virus injection date': '2019-12-19 00:00:00',
-                   'VIRUS': 'AAV1-syn-jGCaMP7f-WPRE', 'cannula implant date': '2020-12-19 00:00:00',
-                   'weight at time of implant': '26.6 g'},
-     '4222153.1': {'mouse #': '4222153.1', 'species': 'mus musculus', 'ID': 'F1', 'DOB': '2019-01-17 00:00:00',
-                   'genotype': 'CaMKII-cre hemizygous', 'sex': 'MALE', 'virus injection date': '2019-03-14 00:00:00',
-                   'VIRUS': 'AAV1-CAG-FLEX-GCaMP6f-WPRE', 'cannula implant date': '2019-03-14 00:00:00',
-                   'weight at time of implant': '28.8 g'},
-     '4222153.2': {'mouse #': '4222153.2', 'species': 'mus musculus', 'ID': 'F2', 'DOB': '2019-01-17 00:00:00',
-                   'genotype': 'CaMKII-cre hemizygous', 'sex': 'MALE', 'virus injection date': '2019-03-14 00:00:00',
-                   'VIRUS': 'AAV1-CAG-FLEX-GCaMP6f-WPRE', 'cannula implant date': '2019-03-14 00:00:00',
-                   'weight at time of implant': '29.3 g'},
-     '4222153.3': {'mouse #': '4222153.3', 'species': 'mus musculus', 'ID': 'F3', 'DOB': '2019-01-17 00:00:00',
-                   'genotype': 'CaMKII-cre hemizygous', 'sex': 'MALE', 'virus injection date': '2019-03-14 00:00:00',
-                   'VIRUS': 'AAV1-CAG-FLEX-GCaMP6f-WPRE', 'cannula implant date': '2019-03-14 00:00:00',
-                   'weight at time of implant': '28.0 g'},
-     '4222174.1': {'mouse #': '4222174.1', 'species': 'mus musculus', 'ID': 'F4', 'DOB': '2018-10-29 00:00:00',
-                   'genotype': 'Ai94 hemizygous; CaMKII-cre hemizygous; CaMKII-tTA hemizygous', 'sex': 'FEMALE',
-                   'virus injection date': '2019-04-13 00:00:00', 'VIRUS': 'NONE',
-                   'cannula implant date': '2019-04-13 00:00:00', 'weight at time of implant': '23.1 g'},
-     '4222154.1': {'mouse #': '4222154.1', 'species': 'mus musculus', 'ID': 'F5', 'DOB': '2019-01-07 00:00:00',
-                   'genotype': 'CaMKII-cre hemizygous', 'sex': 'FEMALE', 'virus injection date': '2019-03-13 00:00:00',
-                   'VIRUS': 'AAV1-CAG-FLEX-GCaMP6f-WPRE', 'cannula implant date': '2019-03-13 00:00:00',
-                   'weight at time of implant': '21.4 g'},
-     '4343702.1': {'mouse #': '4343702.1', 'species': 'mus musculus', 'ID': 'F6', 'DOB': '2019-10-29 00:00:00',
-                   'genotype': 'CaMKII-cre hemizygous', 'sex': 'FEMALE', 'virus injection date': '2020-02-20 00:00:00',
-                   'VIRUS': 'AAV1-CAG-FLEX-GCaMP6f-WPRE', 'cannula implant date': '2020-02-20 00:00:00',
-                   'weight at time of implant': '19.0 g'},
-     '4222157.4': {'mouse #': '4222157.4', 'species': 'mus musculus', 'ID': 'FD1', 'DOB': '2019-02-08 00:00:00',
-                   'genotype': 'CaMKII-cre hemizygous', 'sex': 'MALE', 'virus injection date': '2019-05-02 00:00:00',
-                   'VIRUS': 'AAV1-CAG-FLEX-GCaMP6f-WPRE', 'cannula implant date': '2019-05-02 00:00:00',
-                   'weight at time of implant': '26.2 g'},
-     '4222169.1': {'mouse #': '4222169.1', 'species': 'mus musculus', 'ID': 'FD2', 'DOB': '2019-03-03 00:00:00',
-                   'genotype': 'CaMKII-cre hemizygous', 'sex': 'FEMALE', 'virus injection date': '2019-07-10 00:00:00',
-                   'VIRUS': 'AAV1-CAG-FLEX-GCaMP6f-WPRE', 'cannula implant date': '2019-07-10 00:00:00',
-                   'weight at time of implant': '22.3 g'},
-     '4222169.2': {'mouse #': '4222169.2', 'species': 'mus musculus', 'ID': 'FD3', 'DOB': '2019-03-03 00:00:00',
-                   'genotype': 'CaMKII-cre hemizygous', 'sex': 'FEMALE', 'virus injection date': '2019-07-10 00:00:00',
-                   'VIRUS': 'AAV1-CAG-FLEX-GCaMP6f-WPRE', 'cannula implant date': '2019-07-10 00:00:00',
-                   'weight at time of implant': '18.7 g'},
-     '4222169.4': {'mouse #': '4222169.4', 'species': 'mus musculus', 'ID': 'FD4', 'DOB': '2019-03-31 00:00:00',
-                   'genotype': 'CaMKII-cre hemizygous', 'sex': 'FEMALE', 'virus injection date': '2019-07-12 00:00:00',
-                   'VIRUS': 'AAV1-CAG-FLEX-GCaMP6f-WPRE', 'cannula implant date': '2019-07-12 00:00:00',
-                   'weight at time of implant': '21.7 g'}}
