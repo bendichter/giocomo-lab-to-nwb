74dadbc7c52696b0e92cccb93e1913305cb621a8
Saksham Sharda <saksham20.sharda@gmail.com>
2021-02-10 21:24:04 +0530

    pep

--- giocomo_lab_to_nwb/ophys/conversionscript.py
+++ giocomo_lab_to_nwb/ophys/conversionscript.py
@@ -1,6 +1,9 @@
-from .giocomoconverter import GiocomoImagingInterface
 from pathlib import Path
+
 from tqdm import tqdm
+
+from .giocomoconverter import GiocomoImagingInterface
+
 
 def conversion_complete(source_path):
     """
@@ -15,7 +18,7 @@ def conversion_complete(source_path):
     for subject_id_path in source_path.iterdir():
         if subject_id_path.name[:-2].isdigit():
             print(f'converting for subject is:{subject_id_path.name}')
-            file_path_list=tqdm([i for i in subject_id_path.glob('*/*/*.sbx')])
+            file_path_list = tqdm([i for i in subject_id_path.glob('*/*/*.sbx')])
             for file_path in file_path_list:
                 file_path_list.set_postfix(current=f'writing: {file_path.name}')
                 try:
@@ -29,7 +32,7 @@ def conversion_complete(source_path):
                     print(f'could not convert: {e}')
 
 
-def convert_file(sbx_filepath: [Path, str], nwb_save_path: [Path,str] = None):
+def convert_file(sbx_filepath: [Path, str], nwb_save_path: [Path, str] = None):
     """
     Convert a single set of sbx, suite2p and VR data pickled files
     Parameters
@@ -42,4 +45,4 @@ def convert_file(sbx_filepath: [Path, str], nwb_save_path: [Path,str] = None):
     if nwb_save_path is None:
         nwb_save_path = Path.cwd()/'sbx_nwb.nwb'
     gio = GiocomoImagingInterface(sbx_filepath)
-    gio.run_conversion(metadata=gio.get_metadata(), nwbfile_path=str(nwb_save_path),overwrite=True)
+    gio.run_conversion(metadata=gio.get_metadata(), nwbfile_path=str(nwb_save_path), overwrite=True)
