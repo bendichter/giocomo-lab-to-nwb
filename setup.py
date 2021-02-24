b76fd203f17a8a3697df44ffff749d44af5e949f
weiglszonja <wszonja@gmail.com>
2021-02-24 14:04:06 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,24 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
 # Get the long description from the README file
 with open('README.md', 'r') as f:
     long_description = f.read()
 
+# Get requirements
+with open('requirements.txt') as f:
+    install_requires = f.read().strip().split('\n')
+
 setup(
     name='giocomo_lab_to_nwb',
-    version='0.1dev',
-    description='tool to convert giocomo matlab data into NWB:N format',
+    version='0.2.0',
+    description='NWB conversion scripts and tutorials',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author='Kristin Quick',
-    author_email='kristin@scenda.io',
-    #packages=['giocomo_lab_to_nwb'],
+    author='Kristin Quick, Luiz Tauffer, Szonja Weigl and Ben Dichter',
+    author_email='ben.dichter@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['pynwb',
-                      'numpy',
-                      'scipy',
-                      'hdf5storage',
-                      'pytz',
-                      'uuid',
-                      'tkcalendar',
-                      'PyYAML',
-                      'nwbn_conversion_tools']
+    package_data={'': ['*.yml', '*.json']},
+    install_requires=install_requires
 )
