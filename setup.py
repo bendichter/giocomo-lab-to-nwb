a096cdea15291f77793ca334de1eaaa762685c58
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2022-05-02 16:15:25 -0400

    v0.2.0
    v0.2.0 adds the wen21 dataset conversion code to the repo

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='giocomo_lab_to_nwb',
-    version='0.1.0',
+    version='0.2.0',
     description='convert Giocomo Neuropixel data into NWB:N format',
     long_description=long_description,
     long_description_content_type='text/markdown',
